e26a8f40a45ff7436e0c58ab47ce30f6df7fc681
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-08-10 18:00:18 +0530

    Added a GET call to a mock service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit(): void {
+    this.httpService.getData()
+      .subscribe(
+        (data: Response) => console.log(data)
+      );
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpService} from './http.service';
 
 
 @NgModule({
@@ -10,9 +12,9 @@ import {AppComponent} from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
